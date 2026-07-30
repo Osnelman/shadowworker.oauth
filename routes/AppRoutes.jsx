f4c29dcca74b86e96061f2adc58c69bb4d232383
Osnel <osnelnawana@gmail.com>
2026-07-30 12:39:12 +0100

    Ajout protection des routes: redirection vers /login si non connecté

--- routes/AppRoutes.jsx
+++ routes/AppRoutes.jsx
@@ -9,18 +9,19 @@ import Mission from '../pages/Mission'
 import Progress from '../pages/Progress'
 import Badges from '../pages/Badges'
 import NotFound from '../pages/NotFound'
+import ProtectedRoute from '../components/ProtectedRoute'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/course/:lessonId" element={<Course />} />
-      <Route path="/quiz/:lessonId" element={<Quiz />} />
-      <Route path="/mission/:sectionId" element={<Mission />} />
-      <Route path="/result" element={<Result />} />
-      <Route path="/progress" element={<Progress />} />
-      <Route path="/badges" element={<Badges />} />
+      <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+      <Route path="/course/:lessonId" element={<ProtectedRoute><Course /></ProtectedRoute>} />
+      <Route path="/quiz/:lessonId" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
+      <Route path="/mission/:sectionId" element={<ProtectedRoute><Mission /></ProtectedRoute>} />
+      <Route path="/result" element={<ProtectedRoute><Result /></ProtectedRoute>} />
+      <Route path="/progress" element={<ProtectedRoute><Progress /></ProtectedRoute>} />
+      <Route path="/badges" element={<ProtectedRoute><Badges /></ProtectedRoute>} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
